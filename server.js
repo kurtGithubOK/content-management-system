68a9d3d47c2ff41c948375ff91974f4dd789b755
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-11-01 21:15:51 -0700

    Put queries into functions

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 // Imports
 const mysql = require('mysql2');
 
+// Constants for queries.
+const LIST_DEPARTMENTS = 'SELECT * FROM department';
+const LIST_ROLES = 'SELECT * FROM role';
+const LIST_EMPLOYEES = 'SELECT * FROM employee';
+
+const ADD_DEPARTMENT = 'INSERT INTO department (name) VALUES (?)';
+const ADD_ROLE = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)';
+const ADD_EMPLOYEE = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+
+const UPDATE_EMPLOYEE = 'UPDATE employee SET role_id = ? WHERE id = ?';
+
 // Make db connection
 const db = mysql.createConnection(
     {
@@ -12,49 +23,51 @@ const db = mysql.createConnection(
     console.log(`Connected to the cms_db database.`)
 );
 
-// Query department table
-const LIST_DEPARTMENTS = 'SELECT * FROM department';
-const LIST_ROLES = 'SELECT * FROM role';
-const LIST_EMPLOYEES = 'SELECT * FROM employee';
+const addDepartment = (name) => {
+    db.query(ADD_DEPARTMENT, [name], (err, result) => {
+        if (err) console.log(`Error occurred when adding department ${name}:`, err);
+        else console.log(`Successfully added department ${name}:`, result);
+    });
+};
 
-const ADD_DEPARTMENT = 'INSERT INTO department (name) VALUES (?)';
-const ADD_ROLE = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)';
-const ADD_EMPLOYEE = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+const listDepartments = () => {
+    db.query(LIST_DEPARTMENTS, function (err, results) {
+        console.log('LIST_DEPARTMENTS results:', results);
+    });
+};
 
-const UPDATE_EMPLOYEE = 'UPDATE employee SET role_id = ? WHERE id = ?';
+const addRole = (role, salary, departmentId) => {
+    db.query(ADD_ROLE, [role, salary, departmentId], (err, result) => {
+        if (err) console.log(`Error occurred when adding role ${role}:`, err);
+        else console.log(`Successfully added role ${role}:`, result);
+    });
+};
 
+const listRoles = () => {
+    db.query(LIST_ROLES, function (err, results) {
+        console.log('LIST_ROLES results:', results);
+    });
+};
 
-// db.query(ADD_DEPARTMENT, ['department 12'], (err, result) => {
-//     if (err) console.log(err);
-//     else console.log(result);
-// });
-// db.query(LIST_DEPARTMENTS, function (err, results) {
-//     console.log('LIST_DEPARTMENTS:', results);
-// });
+const addEmployee = (firstName, lastName, roleId, managerId) => {
+    db.query(ADD_EMPLOYEE, [firstName, lastName, roleId, managerId], (err, result) => {
+        if (err) console.log(`Error occurred when adding employee ${lastName}:`, err);
+        else console.log(`Successfully added employee ${lastName}:`, result);
+    });
+};
 
-// db.query(ADD_ROLE, ['queryRole', 22000, 1], (err, result) => {
-//     if (err) console.log(err);
-//     else console.log(result);
-// });
-// db.query(LIST_ROLES, function (err, results) {
-//     console.log('LIST_ROLES:', results);
-// });
+const listEmployees = () => {
+    db.query(LIST_EMPLOYEES, function (err, results) {
+        console.log('LIST_EMPLOYEES:', results);
+    });
+};
 
-// db.query(ADD_EMPLOYEE, ['kurtsxxxx', 'heimermansxxxx', 2, 1], (err, result) => {
-//     if (err) console.log('Error:', err);
-//     else console.log('This is result:', result);
-// });
-// db.query(LIST_EMPLOYEES, function (err, results) {
-//     console.log('LIST_EMPLOYEES:', results);
-// });
-
-// db.query(UPDATE_EMPLOYEE, [1, 5], (err, result) => {
-//     if (err) console.log('Error:', err);
-//     else console.log('This is result:', result);
-// });
-// db.query(LIST_EMPLOYEES, function (err, results) {
-//     console.log('LIST_EMPLOYEES:', results);
-// });
+const updateEmployee = (roleId, employeeId) => {
+    db.query(UPDATE_EMPLOYEE, [roleId, employeeId], (err, result) => {
+        if (err) console.log(`Error occurred when updating employeeId ${employeeId}:`, err);
+        else console.log(`Successfully updated employeeId ${employeeId}:`, result);
+    });
+};
 
 
 
