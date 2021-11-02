519322074f9dc38c867ce5271b21d015cd5a7881
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-11-01 19:11:53 -0700

    Add .gitignore & package.json.  Start server.js

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+// Imports
+const mysql = require('mysql2');
+
+// Make db connection
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'cms_db'
+    },
+    console.log(`Connected to the cms_db database.`)
+  );
+  
+
+
+
 console.log('hi')
 
 
